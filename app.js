603d4014a817f1a93f7280c9192b9ee41a23e09e
Sushant <sushant.malvadakar@thinkitive.com>
2022-06-29 17:33:48 +0530

    city name

--- app.js
+++ app.js
@@ -13,7 +13,16 @@ app.get('/', function(req, res){
 app.post('/', function(req, res) {
     let city = req.body.city;
     const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +",india&units=metric&appid={{Enter ur unique app id here}}";
-    
+    // https.get(url, function(response){
+    //     console.log(res.statusCode);
+    //     const weatherData = JSON.parse(data)
+    //     const temp = weatherData.main.temp;
+    //     console.log("temperature in Pune is "+ temp+ " degree celcius.")
+        // res.on("data", function(data){
+        //     const temp = weatherData.main.temp;
+        //     console.log("temperature in Pune is "+ temp+ " degree celcius.")
+        // })
+    // })
 })
 
 
