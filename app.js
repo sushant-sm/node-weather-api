c64465be4e29664268cedbc28bc0e388ca0707e8
Sushant <sushant.malvadakar@thinkitive.com>
2022-06-29 17:30:32 +0530

    get city name

--- app.js
+++ app.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
+const https = require("https");
+const { stat } = require('fs');
 
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/', function(req, res){
+    res.sendFile(__dirname+ "/index.html")
+});
+
+app.post('/', function(req, res) {
+    let city = req.body.city;
+    const url = "https://api.openweathermap.org/data/2.5/weather?q="+ city +",india&units=metric&appid={{Enter ur unique app id here}}";
+    
+})
+
+
+app.listen(3000, function() {
+    console.log("Listining on 3000");
+})
 
 
